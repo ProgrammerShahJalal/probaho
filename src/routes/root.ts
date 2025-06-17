0218640f1c452dedc0a7c0ea6d707dd9a8c79d56
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-06-17 12:56:03 +0600

    probaho login page optimized

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -21,10 +21,59 @@ module.exports = async function (fastify: FastifyInstance) {
         })
 
         .get(
+            '/super-admin',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/super_admin.ejs');
+            },
+        )
+        .get(
             '/admin',
             { preHandler: check_is_admin_and_redirect },
             async (_req: FastifyRequest, reply: FastifyReply) => {
                 return reply.view('dashboard/admin.ejs');
+            },
+        )
+        .get(
+            '/student',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/student.ejs');
+            },
+        )
+        .get(
+            '/parent',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/parent.ejs');
+            },
+        )
+        .get(
+            '/teacher',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/teacher.ejs');
+            },
+        )
+        .get(
+            '/accountant',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/accountant.ejs');
+            },
+        )
+        .get(
+            '/receptionist',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/receptionist.ejs');
+            },
+        )
+        .get(
+            '/librarian',
+            { preHandler: check_is_admin_and_redirect },
+            async (_req: FastifyRequest, reply: FastifyReply) => {
+                return reply.view('dashboard/librarian.ejs');
             },
         );
 };
