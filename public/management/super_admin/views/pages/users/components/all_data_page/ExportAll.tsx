d7268a6e83c41d2588c486c0c7b6aa3bbef62267
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-07-03 16:26:17 +0600

    feat: add ConfirmModal component for export confirmation and integrate it into ExportAll and ExportSelected components

--- public/management/super_admin/views/pages/users/components/all_data_page/ExportAll.tsx
+++ public/management/super_admin/views/pages/users/components/all_data_page/ExportAll.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { initialState } from '../../config/store/inital_state';
 import setup from '../../config/setup';
 import { RootState } from '../../../../../store';
 import { useSelector } from 'react-redux';
 import { CsvBuilder } from 'filefy';
 import { anyObject } from '../../../../../common_types/object';
+import ConfirmModal from '../../../../components/ConfirmModal';
 
 export interface Props {}
 
 const ExportAll: React.FC<Props> = (props: Props) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const state: typeof initialState = useSelector(
         (state: RootState) => state[setup.module_name],
     );
 
-    function handle_export(e: React.MouseEvent<HTMLElement, MouseEvent>) {
-        e.preventDefault();
+    const confirm_export = () => {
         const columns = [
             'id',
             'uid',
@@ -47,6 +48,14 @@ const ExportAll: React.FC<Props> = (props: Props) => {
             .setColumns(columns)
             .addRows(rows)
             .exportFile();
+        setIsModalOpen(false); // Close modal after export
+    };
+
+    function handle_export_click(
+        e: React.MouseEvent<HTMLElement, MouseEvent>,
+    ) {
+        e.preventDefault();
+        setIsModalOpen(true);
     }
 
     // Conditionally render the button only if there's data to export
@@ -56,12 +65,19 @@ const ExportAll: React.FC<Props> = (props: Props) => {
     }
 
     return (
-        <>
-            <a href="#" onClick={(e) => handle_export(e)}>
+        <div>
+            <a href="#" onClick={(e) => handle_export_click(e)}>
                 <span className="material-symbols-outlined fill">download</span>
                 <div className="text text-white">Export All</div>
             </a>
-        </>
+            <ConfirmModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onConfirm={confirm_export}
+                message="Are you sure you want to export all records?"
+                title="Confirm Export All"
+            />
+        </div>
     );
 };
 
