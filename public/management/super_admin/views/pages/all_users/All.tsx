cef294158e26fda7d1c060a6f41bb78615472a42
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-07-09 18:12:52 +0600

    freshed

--- public/management/super_admin/views/pages/all_users/All.tsx
+++ public/management/super_admin/views/pages/all_users/All.tsx
@@ -73,7 +73,7 @@ const All: React.FC<Props> = () => {
         <div className="page_content">
             <div className="explore_window fixed_size">
                 {/* Changed title to be static */}
-                <Header title={'All Branch Admins'} />
+                <Header title={'All Users'} />
 
                 <div className="content_body">
                     <div className="data_list">
