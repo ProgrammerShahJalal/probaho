ceba2c62c062e5ec7cc229ce14aaf34023288d1f
ProgrammerShahJalal <bestwebdesignerr@gmail.com>
2025-07-15 10:49:11 +0600

    branch infos added server

--- src/database/models.type.ts
+++ src/database/models.type.ts
@@ -1,4 +1,5 @@
 import * as ErrorTraceModel from "../modules/system_management/models/error_trace.model";
+import * as BranchInfosModel from "../modules/branch_infos/models/model";
 import * as UserModel from "../modules/users/models/user_model";
 import * as ContactModel from "../modules/contact_management/models/model";
 import * as UserRolesModel from "../modules/user_roles/models/model";
@@ -9,6 +10,7 @@ import { Sequelize } from "sequelize";
 export type model_types = {
     ErrorTraceModel: typeof ErrorTraceModel.DataModel;
     
+    BranchInfosModel: typeof BranchInfosModel.DataModel;
     ContactModel: typeof ContactModel.DataModel;
     UserModel: typeof UserModel.DataModel;
     UserRolesModel: typeof UserRolesModel.DataModel;
---
+++ src/modules/branch_infos/models/model.ts
@@ -0,0 +1,111 @@
+import {
+    DataTypes,
+    Model,
+    Sequelize,
+    InferAttributes,
+    InferCreationAttributes,
+    CreationOptional,
+} from 'sequelize';
+
+const tableName = 'branch_infos';
+const modelName = 'BranchInfosModel';
+
+type Infer = InferAttributes<DataModel>;
+type InferCreation = InferCreationAttributes<DataModel>;
+
+type status = 'active' | 'deactive';
+
+
+class DataModel extends Model<Infer, InferCreation> {
+    declare id?: CreationOptional<number>;
+    declare user_id: number;
+    declare branch_code: string;
+    declare name: string;
+    declare logo: string;
+    declare address: string;
+    declare primary_contact: string;
+    declare email: string;
+    declare map?: string | null;
+    declare lat?: number | null;
+    declare lng?: number | null;
+    declare infos?: string | null;
+
+    declare status?: status;
+    declare created_at?: CreationOptional<Date>;
+    declare updated_at?: CreationOptional<Date>;
+}
+
+function init(sequelize: Sequelize) {
+    DataModel.init(
+        {
+            id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            user_id: {
+                type: DataTypes.ENUM('branch'),
+                defaultValue: 'branch',
+            },
+            branch_code: {
+                type: DataTypes.STRING,
+                unique: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+            },
+            logo: {
+                type: DataTypes.STRING,
+            },
+            address: {
+                type: DataTypes.STRING,
+            },
+            primary_contact: {
+                type: DataTypes.STRING,
+            },
+            email: {
+                type: DataTypes.STRING,
+            },
+            map: {
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
+            lat: {
+                type: DataTypes.DECIMAL(10, 8),
+                allowNull: true,
+            },
+            lng: {
+                type: DataTypes.DECIMAL(11, 8),
+                allowNull: true,
+            },
+            infos: {
+                type: DataTypes.TEXT,
+                allowNull: true,
+            },
+            status: {
+                type: DataTypes.ENUM('active', 'deactive'),
+                defaultValue: 'active'
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: DataTypes.NOW,
+            },
+        },
+        {
+            tableName: tableName,
+            modelName: modelName,
+            sequelize, // Passing the `sequelize` instance is required
+            underscored: true,
+        },
+    );
+
+    return DataModel;
+}
+
+export { init, DataModel, modelName };
